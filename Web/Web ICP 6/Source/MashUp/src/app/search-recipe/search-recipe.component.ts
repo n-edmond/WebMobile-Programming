59b85fd807822c3eb2bca0189e4a53a952d0ac0d
n-edmond <nehesia.i.edmond@gmail.com>
2020-10-01 21:26:37 -0500

    more edits to code. Part not working yet

--- Web/Web ICP 6/Source/MashUp/src/app/search-recipe/search-recipe.component.ts
+++ Web/Web ICP 6/Source/MashUp/src/app/search-recipe/search-recipe.component.ts
@@ -27,6 +27,7 @@ export class SearchRecipeComponent implements OnInit {
   geolocationPosition: any;
 
 
+
   constructor(private _http: HttpClient) {
   }
 
@@ -36,7 +37,6 @@ export class SearchRecipeComponent implements OnInit {
   private rcredentials = 'app_id=33bd6a80&app_key=2c385b95519f0dfd3456a6c4eefc87ed';
 
   ngOnInit() {
-
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.geolocationPosition = position;
@@ -54,26 +54,27 @@ export class SearchRecipeComponent implements OnInit {
 
     if (this.recipeValue !== null) {
 
+      // used to excuse the limit error
       // tslint:disable-next-line:max-line-length
       this._http.get('https://api.edamam.com/search?q=' + this.recipeValue + '&' + this.rcredentials + '&from=0&to=10&calories=591-722&health=alcohol-free'
       ).subscribe(result => {
-        this.recipeList = result['hits'];
+        this.recipeList = result['hits']; // saves the reciepe list as an obj.
 
-        console.log(this.recipeList);
+        console.log(this.recipeList); // displays the recipe list as an object. used for testing
         },
-        error => {
+        error => { // error handling. nothing here.
       });
     }
-
+/*the same process as above is done here. We are instead acccessing venue locations similar to the food value*/
     if (this.placeValue != null && this.placeValue !== '' && this.recipeValue != null && this.recipeValue !== '') {
       // tslint:disable-next-line:max-line-length
-      this._http.get('https://api.foursquare.com/v2/venues/explore' + this.credentials +  '&v=20173009&&limit=10m&near='+ this.placeValue + '&query=' + this.recipeValue
-    ).subscribe(result => {
-          this.venueList.push(result['response']);
-          console.log(this.venueList);
-          console.log(result);
+      this._http.get('https://api.foursquare.com/v2/venues/explore&' + this.credentials +  '&v=20173009&&limit=10m&near=' + this.placeValue + '&query=' + this.recipeValue +
+    '').subscribe(result => {
+          this.venueList = result['response'];
+          console.log(this.venueList); // displays for testing
+          console.log(result); // displays for testing.
         },
-        error => {
+        error => { // error handling here
 
         });
 
