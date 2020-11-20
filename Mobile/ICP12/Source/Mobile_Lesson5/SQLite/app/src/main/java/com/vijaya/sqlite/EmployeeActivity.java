a8c9b1c3b1c6b9e405dce7f2ad6473a75e111208
N. Edmond <65703813+n-edmond@users.noreply.github.com>
2020-11-19 21:23:55 -0800

    Both functional. Need to add images

--- Mobile/ICP12/Source/Mobile_Lesson5/SQLite/app/src/main/java/com/vijaya/sqlite/EmployeeActivity.java
+++ Mobile/ICP12/Source/Mobile_Lesson5/SQLite/app/src/main/java/com/vijaya/sqlite/EmployeeActivity.java
@@ -22,6 +22,8 @@ public class EmployeeActivity extends AppCompatActivity {
     private ActivityEmployeeBinding binding;
     private static final String TAG = "EmployeeActivity";
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -127,63 +129,72 @@ public class EmployeeActivity extends AppCompatActivity {
     }
 
     /**This section will delete the information from the DB ICP 12 TODO Completed here
-     *
      */
     private void deleteFromDB(){
-        //attempting to connect to the DB
+       //setting first and last name variables
+        String fname = binding.firstnameEditText.getText().toString();
+        String lname = binding.lastnameEditText.getText().toString();
+
+        //attempting to connect to db
         SQLiteDatabase database = new SampleDBSQLiteHelper(this).getReadableDatabase();
-        //Get the name of the employee to be deleted
-        String username = binding.firstnameEditText.getText().toString();
-        //calls the delete function. Will delete employees with this name.
-        database.delete(SampleDBContract.Employee.TABLE_NAME, SampleDBContract.Employee.COLUMN_FIRSTNAME + "='" + username + "'",null);
-        // Give a message saying the output worked
-        Toast.makeText(this, "Employee: " + username + "has been deleted", Toast.LENGTH_LONG).show();
+
+        String[] selectionArgs = {"%" + fname + "%", "%" + lname + "%"};
+        Cursor cursor = database.rawQuery(SampleDBContract.SELECT_EMPLOYEE_WITH_EMPLOYER, selectionArgs);
+
+        //placeholder to count id num
+        int id=0;
+        //iterates through db to find info
+        if (cursor != null && cursor.moveToFirst()){
+            do{
+                int mID = cursor.getInt(0);
+                id= mID;
+
+            } while (cursor.moveToNext());
+        }
+        //produce a message informing of deletion.
+        database.delete(SampleDBContract.Employee.TABLE_NAME, "_id="+id, null);
+        Log.i("Information ", Integer.toString(id));
+        Toast.makeText(EmployeeActivity.this, "The record has been deleted for ID number " + id, Toast.LENGTH_LONG).show();
     }
 
     /**This section will update the information in the DB ICP 12 TODO Completed here
-     *
      */
     private void updateInDB(){
         //requires similar formatting for save to database function.
 
-        //attempting to connect to the DB
-        SQLiteDatabase database = new SampleDBSQLiteHelper(this).getReadableDatabase();
-        ContentValues values = new ContentValues();
-        //get name of the employee to be updated
-        String username = binding.firstnameEditText.getText().toString();
+        //variables for name created here
+        String fname = binding.firstnameEditText.getText().toString();
+        String lname = binding.lastnameEditText.getText().toString();
 
-        values.put(SampleDBContract.Employee.COLUMN_LASTNAME, binding.lastnameEditText.getText().toString());
+
+        //attempting to connect to db
+        SQLiteDatabase database = new SampleDBSQLiteHelper(this).getReadableDatabase();
+        //attempting to create a values object
+        ContentValues values = new ContentValues();
+
+        //used for db query
+        String[] names = {"%" + fname + "%", "%" + lname + "%"};
+        //Using cursor since the DB can return more than one row for the search criteria
+        Cursor cursor = database.rawQuery(SampleDBContract.SELECT_EMPLOYEE_WITH_EMPLOYER, names);
+
+        //used to identify row
+        int id=0;
+        //iterates through the db to find the info
+        if (cursor != null && cursor.moveToFirst()){
+            do{
+                int mID = cursor.getInt(0);
+                id= mID;
+            } while (cursor.moveToNext());
+        }
+        //updating information using .put()
         values.put(SampleDBContract.Employee.COLUMN_JOB_DESCRIPTION, binding.jobDescEditText.getText().toString());
         values.put(SampleDBContract.Employee.COLUMN_EMPLOYER_ID,
-                ((Cursor)binding.employerSpinner.getSelectedItem()).getInt(0));
-        Log.d("getINT", ((Cursor)binding.employerSpinner.getSelectedItem()).getInt(0) + "");
-        Log.d("getColumnName", ((Cursor)binding.employerSpinner.getSelectedItem()).getColumnName(0));
-        try {
-            //adding calendar section. Coped from saveToDB function
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime((new SimpleDateFormat("dd/MM/yyyy")).parse(
-                    binding.dobEditText.getText().toString()));
-            long date = calendar.getTimeInMillis();
-            values.put(SampleDBContract.Employee.COLUMN_DATE_OF_BIRTH, date);
+                ((Cursor) binding.employerSpinner.getSelectedItem()).getInt(0));
+        database.update(SampleDBContract.Employee.TABLE_NAME, values, "_id="+id, null);
 
-            calendar.setTime((new SimpleDateFormat("dd/MM/yyyy")).parse(
-                    binding.employedEditText.getText().toString()));
-            date = calendar.getTimeInMillis();
-            values.put(SampleDBContract.Employee.COLUMN_EMPLOYED_DATE, date);
-        }
-        catch (Exception e) {
-            //provides error message if in wrong format
-            Log.e(TAG, "Error", e);
-            Toast.makeText(this, "Date is in the wrong format", Toast.LENGTH_LONG).show();
-            //kills the program
-            return;
-        }
-
-        //updating info here
-        String update = SampleDBContract.Employee.COLUMN_FIRSTNAME + "='" + username + "'";
-        database.update(SampleDBContract.Employee.TABLE_NAME,values ,update,null);
-        //notify user that update has been completed
-        Toast.makeText(this, "Employee: " + username + "'s records were updated.", Toast.LENGTH_LONG).show();
-
+        //gives notification that update has been done
+        Log.i("THIS IS ID",Integer.toString(id));
+        Toast.makeText(EmployeeActivity.this, "The record has been updated for " + fname + "ID " + id , Toast.LENGTH_LONG).show();
+        readFromDB();
     }
 }
